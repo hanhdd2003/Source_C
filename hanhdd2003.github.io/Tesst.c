08f7d3e4fe140173d3ee75f874d8b6e74619da1f
hanhdd2003 <126433394+hanhdd2003@users.noreply.github.com>
2023-04-29 13:23:42 +0700

    Add files via upload

---
+++ hanhdd2003.github.io/Tesst.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main (int argc, char *argv[]){
+printf("                                                          _oo0oo_					\n");              
+		printf("                                                         o8888888o				\n");
+		printf("                                                         88\" . \"88				\n");
+		printf("                                                         (| -_- |)				\n");
+		printf("                                                         0\\  =  /0				\n");
+		printf("                                                       ___/`---'\\___				\n");
+		printf("                                                     .' \\\\|     |// '.			\n");			
+		printf("                                                     / \\|||  :  |||/ \\			\n");
+		printf("                                                   / _||||| -:- |||||-\\			\n");
+		printf("                                                  |   | \\\\\\  -  /// |  |			\n");
+		printf("                                                  | \\_|  ''\\\---/''  |_/ |			\n");
+		printf("                                                  \\  .-\\__  '-'  ___/-. /			\n");
+		printf("                                                ___'. .'  /--.--\\  `. .'___		\n");	
+		printf("                                               ."" '<  `.___\\_<|>_/___.' >' "".		\n");	
+		printf("                                            | | :  `- \\`.;`\\ _ /`;.`/ - ` : | |	\n");		
+		printf("                                             \\ \ \ `_.   \\_ __\\ /__ _/   .-` /  /	\n");		
+		printf("                                        =====`-.____`.___ \\_____/___.-`___.-'=====\n");		
+		printf("                                                          `=---='					\n");			
+		printf("                                   												\n");			
+		printf("                                        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");			
+		printf("                                               Phat phu ho, khong bao gio BUG		\n");			
+		printf("                                        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");	
+
+
+
+	return 0;
+}
+
